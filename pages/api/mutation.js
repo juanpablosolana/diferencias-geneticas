63d84f8b7730743bc2902061bda12595baa74796
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2021-12-16 08:41:12 -0600

    new array validation

--- pages/api/mutation.js
+++ pages/api/mutation.js
@@ -1,4 +1,21 @@
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'Pablo Solana' })
+  // validar que la petición solo se realice por el método POST
+  if (req.method !== 'POST') return res.status(403).send()
+
+  // función principal
+  function hasMutation(dna) {
+    // solo se puede aceptar un ADN con estas letras
+    const dnaString = ['A', 'T', 'G', 'C']
+    const dnaSplit = dna.map(letter => letter.toUpperCase().split(''))
+    const dnaFilter = dnaSplit.map(letters => letters.filter(letter => dnaString.includes(letter) ? letter : (res.status(403).send())))
+    console.log(dnaFilter)
+
+    res.status(200).json(dnaFilter)
+
+  }
+
+  // invocar la función principal enviando los parámetros obtenidos desde la petición
+  hasMutation(req.body.dna)
+
 }
