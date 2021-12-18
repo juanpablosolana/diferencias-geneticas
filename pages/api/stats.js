c25ffdc0a9ca37206598c9de2cbcccab33f19ca5
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2021-12-17 21:21:57 -0600

    new ratio response

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -10,23 +10,23 @@ export default async function handler(req, res) {
   switch (method) {
     case 'GET':
       try {
-          const mutations =  await Mutation.find({})
+        const mutations = await Mutation.findOne({ _id: '61bcf4515da94c232171fbfc'})
           // console.log(mutations)
         res.status(200).json(mutations)
         } catch (error) {
           res.status(400).json(error)
         }
       break
-    case 'POST':
-      try {
-        const mutation =  Mutation.create(
-          req.body
-        ) /* create a new model in the database */
-        res.status(201).json({ success: true, data: mutation })
-      } catch (error) {
-        res.status(400).json({ success: false })
-      }
-      break
+    // case 'POST':
+    //   try {
+    //     const mutation =  Mutation.create(
+    //       req.body
+    //     ) /* create a new model in the database */
+    //     res.status(201).json({ success: true, data: mutation })
+    //   } catch (error) {
+    //     res.status(400).json({ success: false })
+    //   }
+    //   break
     default:
       res.status(400).json({ success: false })
       break
