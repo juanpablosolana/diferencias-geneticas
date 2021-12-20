0c68c095786e0cf96d8171eeeb81d2df819fe46e
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2021-12-19 19:34:37 -0600

    letters validation

--- pages/api/mutation.js
+++ pages/api/mutation.js
@@ -1,10 +1,12 @@
 import isFilter from "../../services/filter" // la función verifica las condiciones del filtro
 import { leftEnd, rightEnd, upDown } from "../../services/array" // las funciones de busqueda de patrones
-import mutationUpdate  from "../../services/updater" // actualizar las estadisticas
+import mutationUpdate from "../../services/updater" // actualizar las estadisticas
 
 export default function handler(req, res) {
+  const dna = req.body.dna
+  const letters = ['A', 'T', 'C', 'G']
 
-  if (req.method !== 'POST') return res.status(403).send() //equivalente a app.post('/api/mutation')
+  if (req.method !== 'POST') return res.status(400).send({ error: '400' }) //equivalente a app.post('/api/mutation')
   function hasMutation(dna) { // función principal
     let mutation = false // si se encuentra una mutación se cambia el estado
     /* Comienza la busqueda de mutaciones */
@@ -13,18 +15,24 @@ export default function handler(req, res) {
     const rightToLeft = isFilter([rightEnd(dna).join('')]) // oblicua derecha a izquierda
     const upToDown = isFilter([upDown(dna).join('')]) //vertical
     const result = horizontal.length + leftToRight.length + rightToLeft.length + upToDown.length
-    result>1? mutation = true : null
+    result > 1 ? mutation = true : null
 
     if (mutation) { // actualizando las estadisticas y retornado resultados
       mutationUpdate('mutationUpdate')
-      .then(()=>{res.status(200).end()})
-      .catch(error=>res.status(500).send(error))
-    } else{
+        .then(() => { res.status(200).end() })
+        .catch(error => res.status(500).send(error))
+    } else {
       mutationUpdate('noMutationsUpdate')
-      .then(() => { res.status(403).end() })
-      .catch(error => res.status(500).send(error))
+        .then(() => { res.status(403).end() })
+        .catch(error => res.status(500).send(error))
     }
   }
 
-  hasMutation(req.body.dna) // invocar la función principal enviando los parámetros obtenidos desde la petición
+  const isValid = dna.every(item => item.split('').every(letter => letters.includes(letter)))
+  // el arreglo debe cumplir con la cadena de letras de lo contrario retorna un error
+  if (isValid && dna.length > 0) {
+    hasMutation(dna) // invocar la función principal enviando los parámetros obtenidos desde la petición
+  } else {
+    res.status(400).send({ error: '400' })
+  }
 }
