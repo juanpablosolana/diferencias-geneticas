d4fd19212faf1483d0016f2c69ad77eafadd4e6f
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2021-12-19 23:02:08 -0600

    Home ok

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import dbConnect from '../lib/db';
+import Mutation from '../models/mutation';
 
-export default function Home() {
-
+const  Home=({mutation,noMutation})=> {
+//  console.log(props)
   return (
     <div className={styles.container}>
       <Head>
@@ -16,7 +17,22 @@ export default function Home() {
         <h1 className={styles.title}>
           Diferencias genéticas
         </h1>
+        <p>Mutaciones: {mutation}</p>
+        <p>Sin Mutación: {noMutation}</p>
+        <p>Promedio: {mutation/noMutation}</p>
       </main>
     </div>
   )
 }
+export async function getServerSideProps() {
+  await dbConnect()
+
+  /* find all the data in our database */
+  const result = await Mutation.findOne({ _id: `${process.env.id}` })
+  const mutation = result.count_mutations
+  const noMutation = result.count_no_mutation
+// console.log(mutation,noMutation)
+  return { props: { mutation, noMutation} }
+}
+
+export default Home
